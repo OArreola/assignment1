c6ba15a0355b22cc75ecb23021f46f8e28905cfe
OArreola <orlando_arreola@hotmail.com>
2020-02-05 09:16:49 -0800

    initial version of assignment 1

---
+++ js/scripts.js
@@ -0,0 +1,192 @@
+// //STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+
+// //STEP 2
+// 3.1416 		        		// numeric literal 
+// "This is a string!!"        // string literal 
+// true            			// Boolean literal 
+// null         				// literal null value 
+
+
+// //STEP 3
+// let name = window.prompt("What is your name?");
+// let totalAverage = sumTotal / numberOfSamples;
+
+
+// //STEP 4
+// let userFirstName;
+// let userAddress;
+// let userCity;
+// let userState;
+// let userZipCode;
+// let userAge;
+// let userReferralSource;
+// let userMayWeContactYou;
+
+
+// //STEP 5
+// //Method 1: 
+// let userFirstName;
+// let userAddress;
+// let userCity;
+// userFirstName = "Mike";
+// userAddress = "8355 Aero Dr";
+// userCity = "San Diego";
+
+// //Method 2:
+// let userState = "CA";
+// let userZipCode = "92123";
+// let userAge = 21;
+
+// //Method 3:
+// let userReferralSource = "email list", userMayWeContactYou = true;
+
+
+// //STEP 6
+// let myNumber = 10;
+// window.console.log("My number is " + myNumber);
+
+
+// //STEP 7
+// let isProductAvailable = true;
+// window.console.log(isProductAvailable + ": Product availability");
+
+// let  totalQuantity = 54;
+// window.console.log(totalQuantity + true);
+
+
+// //STEP 8
+// //Original string literal:
+// //var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+// //window.console.log(someString);
+// //someString literal is not valid
+
+// //Fixing string literal:
+// var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+// window.console.log(someString);
+
+
+// //STEP 9
+// let nullValue = null;
+// let undefinedValue = undefined;
+// window.console.log(nullValue);
+// window.console.log(undefinedValue);
+
+
+// //STEP 10
+// window.console.log(typeof'a');
+// window.console.log(typeof 4.5);
+// window.console.log(typeof false);
+// window.console.log(typeof {studentName:"Mike"});
+// window.console.log(typeof newVariable);
+
+
+// //STEP 11
+// window.alert("Hello " + "Orlando Arreola" + ", welcome to the JavaScript class!");
+
+
+// //STEP 12
+// let studentName = "Orlando Arreola";
+// window.alert("Hello " + studentName + ", welcome to the JavaScript class!");
+
+
+// //STEP 13
+// let studentName = "Orlando Arreola";
+// let course = "JavaScript";
+// window.alert("Hello " + studentName + ", welcome to the " + course + " class!");
+
+
+// //STEP 14
+// let studentName = "Orlando Arreola";
+// let course = "JavaScript";
+// window.alert("Hello " + studentName + "\nWelcome to the " + course + " class!");
+
+
+// // STEP 15
+// let studentName = window.prompt("Enter your name:");
+// let course = "JavaScript";
+// window.alert("Hello " + studentName + "\nWelcome to the " + course + " class!");
+
+
+// // STEP 16
+// let studentName = window.prompt("Enter your name:");
+// let course = window.prompt("Which class are you taken?");
+// window.alert("Hello " + studentName + "\nWelcome to the " + course + " class!");
+
+
+// // STEP 17
+// let x = 10;
+// let y = 20;
+// window.console.log(x + " + " + y + " = " + (x+y));
+
+
+// //STEP 18
+// let x = 20;
+// x += 20;
+// window.console.log(" x = " + x);
+
+
+// //STEP 19
+// let x = 20;
+// x *= 5;
+// window.console.log(" x = " + x);
+
+
+// //STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// window.console.log(" x = " + x);
+
+
+// //STEP 21
+// let firstName = "John";
+// let lastName = "Doe"
+// window.console.log(firstName === "John" && lastName === "Doe");
+
+
+// //STEP 22
+// let testScore1 = 80;
+// let testScore2 = 55;
+// window.console.log("Student approved both tests? " +  (testScore1 > 60 && testScore2 > 60));
+
+
+// //STEP 23
+// let widget = new Object();
+// window.console.log(typeof widget);
+
+
+// //STEP 24
+// let widget = new Object();
+// window.console.log(widget instanceof Object);
+
+
+// //STEP 25
+// let widget = new Object();
+// window.console.log(!widget instanceof Object);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
